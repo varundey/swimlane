ee6dd06f690cfc4f076596b269512209badc1bba
Varun Dey <varundey20@gmail.com>
2019-10-07 00:32:24 +0530

    Add style/CSS loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: "./src/index.jsx",
+	entry: "./src/index.js",
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "index.js"
 	},
+	devtool: "source-map",
 	module: {
 		rules: [{
 			test: /\.(js|jsx)$/,
@@ -19,6 +20,9 @@ module.exports = {
 			use: {
 				loader: "html-loader"
 			}
+		}, {
+			test: /\.css$/i,
+			use: ['style-loader', 'css-loader'],
 		}]
 	},
 	plugins: [
