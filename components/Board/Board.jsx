11d135530a17d912a9dc5dcdde74f8101cf5f488
Varun Dey <varundey20@gmail.com>
2020-05-03 01:20:23 +0530

    Add Header to Board

--- components/Board/Board.jsx
+++ components/Board/Board.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { PageHeader } from "antd";
 
 const Board = (props) => {
-  return <div>{props.boardName}</div>;
+  return (
+    <PageHeader
+      className="site-page-header"
+      title={props.boardName}
+      backIcon={false}
+    />
+  );
 };
 
 export default Board;
