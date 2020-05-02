b6d346b44df6f3532c18b0909e2cd0f8fa1115f7
Varun Dey <varundey20@gmail.com>
2020-05-02 23:34:19 +0530

    Add resolver and prettify webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,38 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: "./src/index.js",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "index.js"
-	},
-	devtool: "source-map",
-	module: {
-		rules: [{
-			test: /\.(js|jsx)$/,
-			exclude: /node_modules/,
-			use: {
-				loader: "babel-loader"
-			}
-		}, {
-			test: /\.html$/,
-			use: {
-				loader: "html-loader"
-			}
-		}, {
-			test: /\.css$/i,
-			use: ['style-loader', 'css-loader'],
-		}]
-	},
-	plugins: [
-			new HtmlWebPackPlugin({
-				template: "./views/index.html",
-				filename: "index.html"
-			})
-	]
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./views/index.html",
+      filename: "index.html",
+    }),
+  ],
 };
