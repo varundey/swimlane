75768cafd0be8309bcb8787b56a3bd2e328b2470
Varun Dey <varundey20@gmail.com>
2020-05-03 01:35:59 +0530

    Create Add List component in Board

--- components/Board/Board.jsx
+++ components/Board/Board.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import { PageHeader } from "antd";
+import { Card, PageHeader } from "antd";
+import "./Board.css";
+
+const BoardHeader = (props) => (
+  <PageHeader
+    className="site-page-header"
+    title={props.boardName}
+    backIcon={false}
+  />
+);
+
+const AddListCard = (props) => (
+  <Card className="add-list-card">
+    <p>Add List</p>
+  </Card>
+);
 
 const Board = (props) => {
   return (
-    <PageHeader
-      className="site-page-header"
-      title={props.boardName}
-      backIcon={false}
-    />
+    <>
+      <BoardHeader boardName={props.boardName} />
+      <AddListCard />
+    </>
   );
 };
 
