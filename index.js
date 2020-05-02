92565fa060a0ccf6577fc288bf888c3e85e22a22
Varun Dey <varundey20@gmail.com>
2020-05-02 23:56:34 +0530

    Move index.js to root

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+
+import BoardCreator from "./components/BoardCreator";
+
+ReactDOM.render(
+  <BoardCreator />,
+  document.getElementById("react-template-container")
+);
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Board from "./components/Board.jsx";
-
-ReactDOM.render(<Board />, document.getElementById("react-template-container"));
