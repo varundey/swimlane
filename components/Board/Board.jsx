1e7ba9bcbd5c1154e65c18ac57db6d6d4b23971d
Varun Dey <varundey20@gmail.com>
2020-05-03 00:45:33 +0530

    Create a Board on click and display the name

---
+++ components/Board/Board.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Board = (props) => {
+  return <div>{props.boardName}</div>;
+};
+
+export default Board;
--- components/BoardCreator/BoardCreator.jsx
+++ components/BoardCreator/BoardCreator.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "antd";
 import "./BoardCreator.css";
+import Board from "../Board/Board";
 
 const { Search } = Input;
 
-const BoardCreator = () => (
-  <div className="board-creator">
-    <Search
-      placeholder="Board name"
-      enterButton="Create"
-      size="large"
-      onSearch={(value) => console.log(value)}
-    />
-  </div>
-);
+const BoardCreator = () => {
+  const [isBoardDisplayed, setIsBoardDisplayed] = useState(false);
+  const [boardName, setBoardName] = useState("");
+
+  const CreateBoard = () => (
+    <div className="board-creator">
+      <Search
+        placeholder="Board name"
+        enterButton="Create"
+        size="large"
+        onSearch={(value) => {
+          setIsBoardDisplayed(true);
+          setBoardName(value);
+        }}
+      />
+    </div>
+  );
+
+  return isBoardDisplayed ? <Board boardName={boardName} /> : <CreateBoard />;
+};
 
 export default BoardCreator;
