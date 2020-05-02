a5e15ebb39e752486423a37f85d21bb917cf67a8
Varun Dey <varundey20@gmail.com>
2020-05-03 01:56:07 +0530

    Add functionality to create lists and render them

--- components/Board/Board.jsx
+++ components/Board/Board.jsx
@@ -10,30 +10,44 @@ const BoardHeader = (props) => (
   />
 );
 
-const AddListCard = (props) => {
-  const [isAddListClicked, setAddListClicked] = useState(false);
+const Board = (props) => {
+  const [boardListArr, setBoardListArr] = useState([]);
 
-  const AddListCard = (props) => (
-    <Card
-      size="small"
-      className="add-list-card"
-      onClick={(_) => setAddListClicked(true)}
-    >
-      <p>Add List</p>
-    </Card>
-  );
+  const AddListCard = (props) => {
+    const [isAddListClicked, setAddListClicked] = useState(false);
 
-  const InputListCard = (props) => {
-    return <Input placeholder="List Name" />;
+    const AddListCard = (props) => (
+      <Card
+        size="small"
+        className="add-list-card"
+        onClick={(_) => setAddListClicked(true)}
+      >
+        <p>Add List</p>
+      </Card>
+    );
+
+    const InputListCard = (props) => {
+      return (
+        <Input
+          className="input-list-card"
+          placeholder="List Name"
+          onPressEnter={(event) =>
+            setBoardListArr([...boardListArr, event.target.value])
+          }
+        />
+      );
+    };
+    return isAddListClicked ? <InputListCard /> : <AddListCard />;
   };
 
-  return isAddListClicked ? <InputListCard /> : <AddListCard />;
-};
-
-const Board = (props) => {
   return (
     <>
       <BoardHeader boardName={props.boardName} />
+      {boardListArr.map((listName) => (
+        <Card size="small" className="add-list-card">
+          <p>{listName}</p>
+        </Card>
+      ))}
       <AddListCard />
     </>
   );
