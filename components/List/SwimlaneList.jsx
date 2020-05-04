3131f7a8699fe8d195363ac4df81eda1291ecab0
Varun Dey <varundey20@gmail.com>
2020-05-04 22:02:33 +0530

    Store Lists as uuid keys + Refactor

--- components/List/SwimlaneList.jsx
+++ components/List/SwimlaneList.jsx
@@ -1,43 +1,50 @@
 import React, { useState } from "react";
 import { Card, Input } from "antd";
 import SwimlaneCard from "../Card/SwimlaneCard";
+import { v4 as uuid } from "uuid";
 
 const { CardRenderer, AddCard } = SwimlaneCard;
 
+const AddList = (props) => {
+  const [isAddListClicked, setAddListClicked] = useState(false);
+  const { addList } = props;
+
+  const AddListButton = (props) => (
+    <Card
+      size="small"
+      className="add-list-card"
+      onClick={(_) => setAddListClicked(true)}
+    >
+      <p>Add List</p>
+    </Card>
+  );
+
+  const InputListName = (props) => {
+    const { addList } = props;
+    return (
+      <Input
+        className="input-list-card"
+        placeholder="List Name"
+        onPressEnter={(event) => {
+          const id = uuid();
+          addList({ id, value: event.target.value });
+        }}
+      />
+    );
+  };
+  return isAddListClicked ? (
+    <InputListName addList={addList} />
+  ) : (
+    <AddListButton />
+  );
+};
+
 const SwimlaneList = (props) => {
   const [cardsArr, setCardsArr] = useState([]);
+  const { lists } = props;
 
   const addCardToList = (cardName) => {
     return setCardsArr([...cardsArr, cardName]);
-  };
-
-  const AddList = (props) => {
-    const [isAddListClicked, setAddListClicked] = useState(false);
-
-    const AddListButton = (props) => (
-      <Card
-        size="small"
-        className="add-list-card"
-        onClick={(_) => setAddListClicked(true)}
-      >
-        <p>Add List</p>
-      </Card>
-    );
-
-    const InputListName = (props) => {
-      return (
-        <Input
-          className="input-list-card"
-          placeholder="List Name"
-          onPressEnter={(event) => props.addList(event.target.value)}
-        />
-      );
-    };
-    return isAddListClicked ? (
-      <InputListName addList={props.addList} />
-    ) : (
-      <AddListButton />
-    );
   };
 
   const ListRenderer = (props) => {
@@ -56,9 +63,10 @@ const SwimlaneList = (props) => {
 
   return (
     <div className="list-container">
-      {props.lists.map((listName) => (
-        <ListRenderer key={listName} listName={listName} />
-      ))}
+      {lists.map((list) => {
+        const { id, value } = list;
+        return <ListRenderer key={id} listName={value} />;
+      })}
       <AddList addList={props.addList} />
     </div>
   );
